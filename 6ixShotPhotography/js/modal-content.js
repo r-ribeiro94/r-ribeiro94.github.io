b81e5fdff8eb5f138e79f0fe6edb3840683d9a74
Rogerio <rogerio.ribeiro2894@gmail.com>
2017-03-25 20:35:05 -0400

    semi-completed template for the website

---
+++ 6ixShotPhotography/js/modal-content.js
@@ -0,0 +1,41 @@
+// First Person
+$(".firstperson").click(function() {
+    $("h4#name").html('Ann');
+    $("p#paragraph").load('../text/sampletext.txt')
+    $(".modal").modal();
+});
+
+// Second Person
+$(".secondperson").click(function() {
+    $("h4#name").html('Todd');
+    $("p#paragraph").load('../text/sampletext.txt')
+    $(".modal").modal();
+});
+
+// Third Person
+$(".thirdperson").click(function() {
+    $("h4#name").html('Seth');
+    $("p#paragraph").load('../text/sampletext.txt')
+    $(".modal").modal();
+});
+
+// Fourth Person
+$(".fourthperson").click(function() {
+    $("h4#name").html('Buddy');
+    $("p#paragraph").load('../text/sampletext.txt')
+    $(".modal").modal();
+});
+
+// Fifth Person 
+$(".fifthperson").click(function() {
+    $("h4#name").html('James');
+    $("p#paragraph").load('../text/sampletext.txt')
+    $(".modal").modal();
+});
+
+// Sixth Person 
+$(".sixthperson").click(function() {
+    $("h4#name").html('Guy');
+    $("p#paragraph").load('../text/sampletext.txt')
+    $(".modal").modal();
+});
